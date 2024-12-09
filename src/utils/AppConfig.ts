4714ebe1eab81bed2ba7c8ea315311aa45db9e4d
TheSUNSRise <2416197795@qq.com>
2024-12-10 00:22:38 +0800

    使用 CSS GRID 创建响应式网格布局

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-	site_name: 'AstroPress',
-	title: 'AstroPress | Astro and Tailwind CSS',
-	description: 'Boilerplate built with Astro and Tailwind CSS',
-	author: 'Nicolas',
+	site_name: 'S Blog',
+	title: '',
+	description: '写点什么东西出来',
+	author: 'S',
 	locale_region: 'de-en',
 	locale: 'en'
 };
